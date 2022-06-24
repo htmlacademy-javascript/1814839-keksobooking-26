93feefc375d2729f42e164ae79cc19a6985566d5
lynx-boop <shimukurumhi@gmail.com>
2022-06-24 21:36:18 +0300

    вносит правки по тЗ
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- js/generate-elements.mjs
+++ js/generate-elements.mjs
@@ -6,8 +6,6 @@ const cardTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
 
-//проходя по массиву, для каждого элемента массива мы создаем img с соответствующим src, полученное записываем в popup__photos
-
 const offerType = {
   palace: 'Дворец',
   flat: 'Квартира',
@@ -18,26 +16,37 @@ const offerType = {
 
 const cardsArray = createObjectsArray();
 
+
 cardsArray.forEach((cardElement) => {
   const card = cardTemplate.cloneNode(true);
+
   card.querySelector('.popup__title').textContent = cardElement.offer.title;
   card.querySelector('.popup__text--address').textContent = cardElement.offer.address;
   card.querySelector('.popup__text--price').textContent = `${cardElement.offer.price} ₽/ночь`;
   card.querySelector('.popup__type').textContent = offerType[cardElement.offer.type];
   card.querySelector('.popup__text--capacity').textContent = `${cardElement.offer.rooms} комнаты для ${cardElement.offer.guests} гостей`;
   card.querySelector('.popup__text--time').textContent = `Заезд после ${cardElement.offer.checkin}, выезд до ${cardElement.offer.checkout}`;
-  card.querySelector('.popup__features').textContent = cardElement.offer.features;
+
+  card.querySelector('.popup__features').innerHTML = '';
+  cardElement.offer.features.forEach((feature) => {
+    const li = document.createElement('li');
+    li.classList.add('popup__feature', `popup__feature--${feature}`);
+    card.querySelector('.popup__features').appendChild(li);
+  });
+
   card.querySelector('.popup__description').textContent = cardElement.offer.description;
 
   card.querySelector('.popup__photos').innerHTML = '';
   cardElement.offer.photos.forEach((photo) => {
     const img = document.createElement('img');
+    img.classList.add('popup__photo');
     img.src = photo;
+    img.width = 45;
+    img.height = 40;
     card.querySelector('.popup__photos').appendChild(img);
   });
 
   card.querySelector('.popup__avatar').src = cardElement.author.avatar;
+
   mapCanvas.appendChild(card);
 });
-
-
